687c45c53bba005fc9466cdace1313699cb840e1
119drogba <119drogba@gmail.com>
2025-04-14 12:12:18 +0900

    step_3_1

--- ch_02/step_2_2.py
+++ ch_02/step_2_2.py
@@ -6,11 +6,11 @@ OUT_2_2 = OUT_DIR / f"{Path(__file__).stem}.xlsx"
 
 if __name__ == "__main__":
     result = []
-for xlsx_path in Path(IN_DIR).glob("2024년*월.xlsx"):
-    df_raw = pd.read_excel(xlsx_path, sheet_name="Sheet1",
-                           usecols="B:E", skiprows=2)
-    result.append(df_raw)
+    for xlsx_path in Path(IN_DIR).glob("2024년*월.xlsx"):
+        df_raw = pd.read_excel(xlsx_path, sheet_name="Sheet1",
+                                usecols="B:E", skiprows=2)
+        result.append(df_raw)
 
 
-df_concat = pd.concat(result)
-df_concat.to_excel(OUT_DIR / f"{Path(__file__).stem}.xlsx", index=False)
+    df_concat = pd.concat(result)
+    df_concat.to_excel(OUT_DIR / f"{Path(__file__).stem}.xlsx", index=False)
